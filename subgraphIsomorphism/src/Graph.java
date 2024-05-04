08fb9c8ebc9ad142482aea406ea43d8600f94958
Adrien Trainor <adrien.trainor@centre.edu>
2024-05-04 14:49:49 -0400

    :3

--- subgraphIsomorphism/src/Graph.java
+++ subgraphIsomorphism/src/Graph.java
@@ -1,75 +1,73 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Graph {
 	
 	int[][] adjMatrix;
-	Vertex[] vertices;
+	ArrayList<Vertex> vertices;
 	int size;
-	Vertex[] verticesSorted;
+	ArrayList<Vertex> verticesSorted;
+	Vertex largest;
+	Comparator<Vertex> comp;
 	
 	public Graph(int[][] adjMatrix) {
 		this.adjMatrix = adjMatrix;
 		this.size = adjMatrix.length;
-		// TODO CONVERT MATRIX TO LIST OF VERTICES
+		this.vertices = makeVertices();
+		this.comp = new vxComparator();
+		this.verticesSorted = sortByDegree();
+		this.largest = verticesSorted.get(0);
 	}
 	
-	// TODO
-	public void makeVertices() {
-		// for each row in given adjacency matrix:
-			// this.vertices[i] = new Vertex object
-			// for j in matrix[i].length
-			//		if matrix[i][j] == 1
-			//			vertex.neighbors += j
-			//			vertex.degree += 1
-			// one row is a vertex
+	public ArrayList<Vertex> makeVertices() {
+		ArrayList<Vertex> verts = new ArrayList<Vertex>();
+		for (int i = 0; i < adjMatrix.length; i++) {
+			int degree = 0;
+			ArrayList<Integer> neighbor = new ArrayList<Integer> ();
+			for (int j = i+1; j < adjMatrix.length; j++) {
+				if(this.adjMatrix[i][j] == 1) {
+					degree = degree+1;
+					neighbor.add(j);
+				}		
+			}
+			verts.add(new Vertex(this,i,degree,neighbor));
+		}
+		return verts;
 	}
-	
-	// TODO
-	public void sortByDegree() {
-		// make a copy of this.vertices
-		// sort copy by vertex degree (small->large) using vartex.greaterDegree()
-		// once all vertices sorted:
-		//	this.largest = largest
-		// this.verticesSorted = sorted
-	}
-	
-	/*
-     public int biggestDegree(int[][] graph) {
-        int length = graph.length;
-        int highestDegree = 0;
-        for (int i = 0; i < length; i++){
-            int degreeCount = 0;
-            for (int j = 0; j < length; j++){
-                if (graph[i][j] == 1) {
-                    degreeCount+= 1;
-                }
-            }
-            if (degreeCount > highestDegree){
-                highestDegree = degreeCount;
-            }
-        }
 
-        return highestDegree;
-     }
-  */
+	public static class vxComparator implements Comparator<Vertex> {
+		public int compare(Vertex c1, Vertex c2) {
+			if (c1.degree > c2.degree ) {
+				return 1;
+			} else if (c1.degree < c2.degree) {
+				return -1;
+			} else {
+				return 0;
+			}
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public ArrayList<Vertex> sortByDegree() {
+		ArrayList<Vertex> vers = (ArrayList<Vertex>) this.vertices.clone();
+		vers.sort(comp);
+		return vers;
+	}
      
-     public Boolean isBiggerThan(Graph that){
-        return this.size >= that.size;
-     }
+	public Boolean isBiggerThan(Graph that) {
+		return this.size >= that.size;
+	}
 
-     // TODO
-     public boolean canFit(Graph that) {
-    	 /* call on G: G.canFit(H)
-    	  	calculate whether this graph can fit that graph:
-    	 		for each Vertex in that:
-    	 			if This has a Vertex with degree >= That vertex:
-    	 				remove This.vertex from what we can check
-    	 				remove That.vertex from what we can check
-    	 			else:
-    	 				// there is a vertex in That that won't fit in This, so
-    	 				return false
-    	 */
-    	 return false; // change
-     }
+    public boolean canFit(Graph that) { 
+    	// that is the subgraph
+    	if (this.largest.degree < that.largest.degree) {return false;}
+   	 	for (int i = 0; i < that.size;i++ ) {
+   	 		int big = this.verticesSorted.get(i).degree;
+   	 		int sub = that.verticesSorted.get(i).degree;
+   	 		if (big < sub) {return false;}
+   	 		}
+   	 	return true; // change
+   	 }
      
      public void printGraph() {
         int length = adjMatrix.length;
--- subgraphIsomorphism/src/Optimal.java
+++ subgraphIsomorphism/src/Optimal.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 
 public class Optimal {
 
 	Graph G;
-	Graph H;
-	
+	Graph H; // H is subgraph
+
 	public Optimal(Graph G, Graph H) {
 		this.G = G;
 		this.H = H;
@@ -11,18 +12,64 @@ public class Optimal {
 	
 	// TODO
 	public boolean isSubgraph() {
+		if (!G.canFit(H)) {return false;}
+		ArrayList<Vertex> startpoints = new ArrayList<Vertex>();
+		for(int i = 0; i < H.size ; i++) {
+			if(G.verticesSorted.get(i).degree > H.largest.degree) {
+				startpoints.add(G.verticesSorted.get(i));
+			}
+		}
+		if(!startpoints.get(0).isPromising(H.verticesSorted.get(0))) {
+			startpoints.remove(0);
+		}
+		
+		while(!startpoints.isEmpty()) {
+			
+			ArrayList<Integer> trasub = new ArrayList<Integer>();
+			
+			ArrayList<Integer> trabig = new ArrayList<Integer>();
+			trasub.add(H.verticesSorted.get(0).index);
+			trabig.add(startpoints.get(0).index);
+
+			Checknode check = new Checknode(startpoints.get(0),H.verticesSorted.get(0),trasub,trabig);
+			if (isSubgraph(check) == true) {
+				return true;
+			} else {
+				startpoints.remove(0);
+			}
+		}
 		return false;
 	}
 	
 	// TODO
-	private boolean isSubgraph(Vertex vG, Vertex vH, Vertex[][] map) {
+	private boolean isSubgraph(Checknode check) {
+		// update travel list
+		if (check.vH.sortedneighbor == null) {return true;}
+		ArrayList<Vertex> uncheckedsubnei = new ArrayList<Vertex>();
+		for (Vertex i : check.vH.sortedneighbor) {
+			if(!check.trasub.contains(i.index)) {
+				uncheckedsubnei.add(i);
+			}	
+		}
+		ArrayList<Vertex> avaneighbor = new ArrayList<Vertex>();
+		return false;
+	}
+	
+	public ArrayList<Vertex> avaneighbor(ArrayList<Vertex> bignei, Vertex matchsub){
+		ArrayList<Vertex> avaneighbor = new ArrayList<Vertex>();
+		for ()
+		
+		return avaneighbor;
+	}
+	
+	public boolean checkonebranch() {
 		return false;
 	}
 	
 	// TODO
-	public boolean isComplete(Vertex[][] map) {
+	/*public boolean isComplete(Vertex[][] map) {
 		// return (map == H.matrix)
 		return false; // change
 	}
-
+*/
 }
